8904c4ab4251e7f8b7f34b72ada14b3e89b41a13
J Corley <jfcorley+github@gmail.com>
2016-05-16 16:19:18 -0500

    suppress the entity name error for crud-root

--- blueprints/crud-root/index.js
+++ blueprints/crud-root/index.js
@@ -1,5 +1,11 @@
 module.exports = {
-  description: ''
+  description: '',
+
+  normalizeEntityName: function() {
+    // crud-root is a hardcoded name
+    // this line suppresses errors about entity name
+    return "test";
+  },
 
   // locals: function(options) {
   //   // Return custom template variables here.
